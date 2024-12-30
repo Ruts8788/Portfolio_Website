5f977c154ede69a43ebf5cdce7aaad43ed407248
Rutuja Amrutkar <97623175+Ruts8788@users.noreply.github.com>
2024-12-30 20:32:24 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+    <script> */
+      var typed = new Typed("#element", {
+        strings: ["Software Engineer"],
+        typeSpeed: 80,
+      });
+   
